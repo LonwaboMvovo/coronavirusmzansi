bf7db8d0bf525926b6874f66d9fb11cb147394b5
LonwaboMvovo <44nator@gmail.com>
2020-04-11 20:37:02 +0200

    updated

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ ZAF1926.addEventListener('click', () => {
 const total_cases_graphsChart = new Chart(total_cases_graphs, {
     type: 'line',
     data: {
-        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', ''],
+        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', '', 'Apr 10'],
         datasets: [{
             backgroundColor: 'rgba(0, 0, 0, 0)',
             borderColor: '#2d545e',
@@ -133,7 +133,7 @@ const total_cases_graphsChart = new Chart(total_cases_graphs, {
             pointBorderColor: 'rgba(0, 0, 0, 0)',
             pointBackgroundColor: '#2d545e',
             lineTension: 0.2,
-            data: [1, 1, 2, 3, 7, 7, 13, 16, 24, 38, 61, 64, 85, 116, 150, 202, 240, 274, 402, 554, 709, 927, 1170, 1187, 1280, 1326, 1353, 1380, 1462, 1505, 1585, 1655, 1686, 1749, 1845, 1934]
+            data: [1, 1, 2, 3, 7, 7, 13, 16, 24, 38, 61, 64, 85, 116, 150, 202, 240, 274, 402, 554, 709, 927, 1170, 1187, 1280, 1326, 1353, 1380, 1462, 1505, 1585, 1655, 1686, 1749, 1845, 1934, 2003]
         }]
     },
     options: {
@@ -167,7 +167,7 @@ const total_cases_graphsChart = new Chart(total_cases_graphs, {
 const global_total_cases_graphsChart = new Chart(global_total_cases_graphs, {
     type: 'line',
     data: {
-        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', ''],
+        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', '', 'Apr 10'],
         datasets: [{
             backgroundColor: 'rgba(0, 0, 0, 0)',
             borderColor: 'black',
@@ -176,7 +176,7 @@ const global_total_cases_graphsChart = new Chart(global_total_cases_graphs, {
             pointBorderColor: 'rgba(0, 0, 0, 0)',
             pointBackgroundColor: 'black',
             lineTension: 0.2,
-            data: [98425, 102050, 106099, 109991, 114381, 118948, 126214, 134509, 145416, 156475, 169511, 182431, 198159, 218840, 244985, 275677, 305106, 337586, 379079, 422914, 471471, 532465, 597018, 663779, 724042, 785828, 859620, 936637, 1016734, 1118414, 1203235, 1274653, 1348667, 1433741, 1518126, 1603694]
+            data: [98425, 102050, 106099, 109991, 114381, 118948, 126214, 134509, 145416, 156475, 169511, 182431, 198161, 218843, 244988, 275680, 305132, 337612, 379105, 422940, 471497, 532491, 597044, 663805, 724220, 786006, 859798, 936851, 1016948, 1118684, 1203505, 1275007, 1349051, 1434167, 1518614, 1604252, 1698881]
         }]
     },
     options: {
@@ -211,14 +211,14 @@ const global_total_cases_graphsChart = new Chart(global_total_cases_graphs, {
 const daily_new_graphsChart = new Chart(daily_new_graphs, {
     type: 'bar',
     data: {
-        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', ''],
+        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', '', 'Apr 10'],
         datasets: [{
             backgroundColor: '#2d545e',
             barPercentage: 0.1,
             barThickness: 'flex',
             maxBarThickness: 8,
             minBarLength: 2,
-            data: [1, 0, 1, 1, 4, 0, 6, 3, 8, 14, 23, 3, 21, 31, 34, 52, 38, 34, 128, 152, 155, 218, 243, 17, 93, 46, 27, 27, 82, 43, 80, 70, 31, 63, 96, 89]
+            data: [1, 0, 1, 1, 4, 0, 6, 3, 8, 14, 23, 3, 21, 31, 34, 52, 38, 34, 128, 152, 155, 218, 243, 17, 93, 46, 27, 27, 82, 43, 80, 70, 31, 63, 96, 89, 69]
         }]
     },
     options: {
@@ -252,14 +252,14 @@ const daily_new_graphsChart = new Chart(daily_new_graphs, {
 const global_daily_new_graphsChart = new Chart(global_daily_new_graphs, {
     type: 'bar',
     data: {
-        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', ''],
+        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', '', 'Apr 10'],
         datasets: [{
             backgroundColor: 'black',
             barPercentage: 0.5,
             barThickness: 6,
             maxBarThickness: 8,
             minBarLength: 2,
-            data: [3111, 3625, 4049, 3892, 4390, 4567, 7266, 8295, 10907, 11059, 13036, 12920, 15728, 20681, 26145, 30692, 29429, 32480, 41493, 43835, 48557, 60994, 64553, 66761, 60263, 61786, 73792, 77017, 80097, 101680, 84821, 71418, 74014, 85074, 84385, 85568]
+            data: [3111, 3625, 4049, 3892, 4390, 4567, 7266, 8295, 10907, 11059, 13036, 12920, 15730, 20682, 26145, 30692, 29452, 32480, 41493, 43835, 48557, 60994, 64553, 66761, 60415, 61786, 73792, 77053, 80097, 101736, 84821, 71502, 74044, 85116, 84447, 85638, 94629]
         }]
     },
     options: {
@@ -294,7 +294,7 @@ const global_daily_new_graphsChart = new Chart(global_daily_new_graphs, {
 const active_graphsChart = new Chart(active_graphs, {
     type: 'line',
     data: {
-        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', ''],
+        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', '', 'Apr 10'],
         datasets: [{
             backgroundColor: 'rgba(0, 0, 0, 0)',
             borderColor: '#2d545e',
@@ -303,7 +303,7 @@ const active_graphsChart = new Chart(active_graphs, {
             pointBorderColor: 'rgba(0, 0, 0, 0)',
             pointBackgroundColor: '#2d545e',
             lineTension: 0.2,
-            data: [1, 1, 2, 3, 7, 7, 13, 16, 24, 38, 61, 64, 85, 116, 150, 202, 238, 272, 398, 550, 697, 915, 1138, 1155, 1247, 1292, 1317, 1325, 1407, 1401, 1481, 1549, 1579, 1641, 1732, 1821]
+            data: [1, 1, 2, 3, 7, 7, 13, 16, 24, 38, 61, 64, 85, 116, 150, 202, 238, 272, 398, 550, 697, 915, 1138, 1155, 1247, 1292, 1317, 1325, 1407, 1401, 1481, 1549, 1579, 1641, 1732, 1821, 1569]
         }]
     },
     options: {
@@ -337,7 +337,7 @@ const active_graphsChart = new Chart(active_graphs, {
 const global_active_graphsChart = new Chart(global_active_graphs, {
     type: 'line',
     data: {
-        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', ''],
+        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', '', 'Apr 10'],
         datasets: [{
             backgroundColor: 'rgba(0, 0, 0, 0)',
             borderColor: 'black',
@@ -346,7 +346,7 @@ const global_active_graphsChart = new Chart(global_active_graphs, {
             pointBorderColor: 'rgba(0, 0, 0, 0)',
             pointBackgroundColor: 'black',
             lineTension: 0.2,
-            data: [39433, 40947, 42328, 43886, 46300, 48031, 53279, 59168, 67413, 74717, 85538, 95640, 107556, 124548, 146790, 172715, 196597, 224316, 260491, 295122, 335940, 384424, 436577, 490926, 538626, 582623, 639139, 695218, 751288, 830832, 891976, 941753, 995294, 1049477, 1099309, 1151564]
+            data: [39433, 40947, 42328, 43886, 46300, 48031, 53279, 59168, 67413, 74717, 85538, 95640, 107558, 124551, 146793, 172718, 196623, 224342, 260517, 295148, 335966, 384450, 436603, 490952, 538797, 582794, 639310, 695425, 751495, 831090, 892234, 942093, 995663, 1049886, 1099777, 1152101]
         }]
     },
     options: {
@@ -381,7 +381,7 @@ const global_active_graphsChart = new Chart(global_active_graphs, {
 const total_deaths_graphsChart = new Chart(total_deaths_graphs, {
     type: 'line',
     data: {
-        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', ''],
+        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', '', 'Apr 10'],
         datasets: [{
             backgroundColor: 'rgba(0, 0, 0, 0)',
             borderColor: '#2d545e',
@@ -390,7 +390,7 @@ const total_deaths_graphsChart = new Chart(total_deaths_graphs, {
             pointBorderColor: 'rgba(0, 0, 0, 0)',
             pointBackgroundColor: '#2d545e',
             lineTension: 0.2,
-            data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 3, 5, 5, 5, 9, 9, 11, 12, 13, 18, 18]
+            data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 3, 5, 5, 5, 9, 9, 11, 12, 13, 18, 18, 24]
         }]
     },
     options: {
@@ -424,7 +424,7 @@ const total_deaths_graphsChart = new Chart(total_deaths_graphs, {
 const global_total_deaths_graphsChart = new Chart(global_total_deaths_graphs, {
     type: 'line',
     data: {
-        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', ''],
+        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', '', 'Apr 10'],
         datasets: [{
             backgroundColor: 'rgba(0, 0, 0, 0)',
             borderColor: 'black',
@@ -433,7 +433,7 @@ const global_total_deaths_graphsChart = new Chart(global_total_deaths_graphs, {
             pointBorderColor: 'rgba(0, 0, 0, 0)',
             pointBackgroundColor: 'black',
             lineTension: 0.2,
-            data: [3387, 3494, 3599, 3827, 4025, 4296, 4628, 4981, 5428, 5833, 6520, 7162, 7979, 8951, 10030, 11386, 13011, 14640, 16514, 18895, 21283, 24074, 27345, 30863, 34067, 37776, 42313, 47203, 53182, 58897, 64696, 69433, 74660, 82046, 88460, 95693]
+            data: [3387, 3494, 3599, 3827, 4025, 4296, 4628, 4981, 5428, 5833, 6520, 7162, 7979, 8951, 10030, 11386, 13011, 14640, 16514, 18895, 21283, 24074, 27345, 30863, 34074, 37783, 42320, 47210, 53189, 58909, 64708, 69447, 74678, 82063, 88480, 95714, 102687]
         }]
     },
     options: {
@@ -468,14 +468,14 @@ const global_total_deaths_graphsChart = new Chart(global_total_deaths_graphs, {
 const daily_deaths_graphsChart = new Chart(daily_deaths_graphs, {
     type: 'bar',
     data: {
-        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', ''],
+        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', '', 'Apr 10'],
         datasets: [{
             backgroundColor: '#2d545e',
             barPercentage: 0.5,
             barThickness: 6,
             maxBarThickness: 8,
             minBarLength: 2,
-            data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 0, 0, 4, 0, 2, 1, 1, 5, 0]
+            data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 0, 0, 4, 0, 2, 1, 1, 5, 0, 6]
         }]
     },
     options: {
@@ -509,14 +509,14 @@ const daily_deaths_graphsChart = new Chart(daily_deaths_graphs, {
 const global_daily_deaths_graphsChart = new Chart(global_daily_deaths_graphs, {
     type: 'bar',
     data: {
-        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', ''],
+        labels: ['Mar 05', '', '', 'Mar 08', '', '', 'Mar 11', '', '', 'Mar 14', '', '', 'Mar 17', '', '', 'Mar 20', '', '', 'Mar 23', '', '', 'Mar 26', '', '', 'Mar 29', '', '', 'Apr 01', '', '', 'Apr 04', '', '', 'Apr 07', '', '', 'Apr 10'],
         datasets: [{
             backgroundColor: 'black',
             barPercentage: 0.5,
             barThickness: 6,
             maxBarThickness: 8,
             minBarLength: 2,
-            data: [102, 107, 105, 228, 198, 271, 332, 353, 447, 405, 687, 642, 817, 972, 1079, 1356, 1625, 1629, 1874, 2381, 2388, 2791, 3271, 3518, 3204, 3709, 4537, 4890, 5979, 5715, 5799, 4737, 5230, 7383, 6414, 7233]
+            data: [102, 107, 105, 228, 198, 271, 332, 353, 447, 405, 687, 642, 817, 972, 1079, 1356, 1625, 1629, 1874, 2381, 2388, 2791, 3271, 3518, 3211, 3709, 4537, 4890, 5979, 5720, 5799, 4739, 5231, 7385, 6417, 7234, 6973]
         }]
     },
     options: {
