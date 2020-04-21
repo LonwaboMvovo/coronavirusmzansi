39402c4bd35ea25d2362975132b8b3460f7ffbee
LonwaboMvovo <44nator@gmail.com>
2020-04-21 12:36:35 +0200

    no background for menu before its clicked

--- menu.js
+++ menu.js
@@ -1,9 +1,12 @@
 const menu_container = document.querySelector('.menu_container');
 const popup_menu = document.querySelector('.popup_menu');
+const full_menu_container = document.querySelector('.full_menu_container');
 
 const showMenu = () => {
     menu_container.classList.toggle('change');
     popup_menu.classList.toggle('nope');
+    full_menu_container.classList.toggle('full_menu_container_background');
+
 }
 
 menu_container.addEventListener('click', showMenu)
